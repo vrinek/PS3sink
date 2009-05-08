1984d9ec50533c82217946d55980c32515c2c77c
Kostas Karachalios <kostas.karachalios@me.com>
2009-05-09 01:56:19 +0300

    full mkv -> mp4 workflow is functional renaming logic has to be refined

--- ps3sink.rb
+++ ps3sink.rb
@@ -2,7 +2,6 @@ class Ps3sink
   DEBUG = false
   
   SPLIT = '-split'
-  
   FOUR_GB = 1024**3*4 # file limit of non 64bit filesystems
   
   INFO = {
@@ -17,17 +16,21 @@ class Ps3sink
     :channels => /Channels/
   }
   
-  ROOT = "/Users/vrinek/Movies/film/"
-  FILENAME = `ls '#{ROOT}'`.split(/\n/).select{|f| f =~ /mkv$/}.reject{|f| f =~ /#{SPLIT}.\d+\.mkv/}[0]
-  FILE = (ROOT + FILENAME)
+  def initialize(root)
+    @root = root.gsub(/([^\/])$/, '\\1/')
+    filename = `ls '#{@root}'`.split(/\n/).select{|f| f =~ /mkv$/}.reject{|f| f =~ /#{SPLIT}.\d+\.mkv/}[0]
+    puts @file = (@root + filename)
+  end
   
   def check_for_programs
     for prog in %w(ffmpeg mkvmerge mkvextract mkvinfo)
       raise "Could not find #{prog} executable" unless system("which #{prog}")
     end
+    
+    raise 'Could not find mp4box or mp4creator' unless system('which mp4box') or system('mp4creator')
   end
   
-  def mkvinfo(file = FILE)
+  def mkvinfo(file = @file)
     cmd = "mkvinfo '#{file}'"
     tracks = `#{cmd}`.split(/\n/).reject{ |l|
       l =~ /^\|?\+/
@@ -55,12 +58,16 @@ class Ps3sink
     puts 'Got info from mkv'
   end
   
-  def split(file = FILE)
+  def video_track
+    @tracks.select{|t| t[:type] == 'video'}[0]
+  end
+  
+  def split(file = @file)
     if (size = File.size(file)) > FOUR_GB
       unless (splits = ls(/#{SPLIT}.\d+\.mkv/)).empty?
         puts 'Found splits, checking size...'
         sum = 0
-        splits.each{|s| sum += File.size(ROOT + s)}
+        splits.each{|s| sum += File.size(@root + s)}
         
         if sum < size
           puts 'Total filesize does not match, need to split again'
@@ -81,6 +88,7 @@ class Ps3sink
         system cmd
       else
         puts 'File is already split'
+        return true
       end
     else
       puts 'Filesize less than 4GB. No need to split'
@@ -88,22 +96,83 @@ class Ps3sink
     end
   end
 
-  def get_audio(file = FILE)
-    cmd = "ffmpeg -i '#{file}' -vn -ac 2 -acodec libfaac -ab 128k '#{ROOT}audio.aac'"
+  def get_audio(file = @file)
+    cmd = "ffmpeg -i '#{file}' -vn -ac 2 -acodec libfaac -ab 128k -threads 2 '#{@root}audio.aac'"
+    system cmd
+  end
+  
+  def get_video(file = @file)
+    cmd = "mkvextract tracks '#{file}' #{video_track[:number]}:'#{@root}video.h264'"
+    system cmd
+  end
+  
+  def correct_video_profile
+    puts 'Correcting the video profile...'
+    v = File.open(@root+'video.h264', 'r+')
+    v.seek 7
+    v.putc 0x29
+    v.close
+  end
+  
+  def mux_mp4
+    if system("which mp4box")
+      puts 'Creating mp4 using mp4box...'
+      cmd = "mp4box -add '#{@root}video.h264' -add '#{@root}audio.aac' -fps #{video_track[:fps]} -hint '#{@root}file.mp4'"
+      system cmd
+    elsif system("which mp4creator")
+      puts 'Adding the video to the mp4...'
+      cmd = "mp4creator -create='#{@root}video.h264' -rate=#{video_track[:fps]} '#{@root}file.mp4'"
+      system cmd
+
+      puts 'Hinting the mp4...'
+      cmd = "mp4creator -hint=1 '#{@root}file.mp4'"
+      system cmd
+
+      puts 'Adding the audio, interleaving and optimising...'
+      cmd = "mp4creator -c '#{@root}audio.aac' -interleave -optimize '#{@root}file.mp4'"
+      system cmd
+    end
+  end
+  
+  def rename_mp4(file = nil)
+    puts (new_name = @root.split(/\//).last.gsub(/ +NZB$/, '').gsub(/\.?(720p|1080p).*?$/i, '').gsub(/\./, ' ').gsub(/(s\d{2}e\d{2}|\d{4})/i, '- \\1')).inspect
+    
+    if file =~ /#{SPLIT}/
+      new_name += '-' + file.scan(/#{SPLIT}.(\d+)/).to_s
+    end
+    
+    cmd = "mv '#{@root}file.mp4' '#{@root+new_name}.mp4'"
     system cmd
   end
   
   def automagick
+    check_for_programs unless DEBUG
+    
     mkvinfo
+    puts @tracks.inspect
+    
     if split
-      puts @files = ls(/#{SPLIT}.\d+\.mkv$/)
+      puts @files = ls(/#{SPLIT}.\d+\.mkv$/).collect{|f| @root + f}
     else
-      @files = [FILE]
+      @files = [@file]
     end
+    
+    @files.each{ |file|
+      get_audio file
+      get_video file
+      correct_video_profile
+      mux_mp4
+      cleanup :audio
+      cleanup :video
+      rename_mp4(file)
+    }
+    
+    cleanup :mkv
+    cleanup :splits
   end
   
   def ls(select = nil, reject = nil)
-    ls = `ls '#{ROOT}'`.split(/\n/)
+    ls = `ls '#{@root}'`.split(/\n/)
     ls = ls.select{|f| f =~ select} if select
     ls = ls.reject{|f| f =~ reject} if reject
     return ls
@@ -116,10 +185,13 @@ class Ps3sink
       puts 'Trashing the splits...'
       trash ls(split_regexp)
     when :mkv
+      puts 'Trashing the original MKV...'
       trash ls(/\.mkv$/, split_regexp)
     when :audio
+      puts 'Trashing the extracted audio'
       trash ls(/^audio\.(aac|m4a)$/)
     when :video
+      puts 'Trashing the extracted video'
       trash ls(/^video\.h264$/)
     when :all
       cleanup :splits
@@ -131,7 +203,7 @@ class Ps3sink
   
   def trash(array_or_filename)
     [array_or_filename].flatten.each{ |file|
-      cmd = "mv '#{ROOT}#{file}' ~/.Trash/"
+      cmd = "mv '#{@root+file}' ~/.Trash/"
       system cmd
     }
   end
@@ -143,5 +215,8 @@ class Ps3sink
   end
 end
 
-ps = Ps3sink.new
+# ROOT = "/Users/vrinek/Downloads/How.I.Met.Your.Mother.S04E22.720p.HDTV.X264-DIMENSION  NZB/"
+ROOT = "/Users/vrinek/Movies/ps3"
+
+ps = Ps3sink.new ROOT
 ps.automagick
